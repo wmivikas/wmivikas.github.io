feb98e3533a133e90e82a6023f60f4495a7cde8c
wmivikas <ownway.vikas@gmail.com>
2026-03-25 16:49:44 +0530

    Merge hero about research into one section file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 const sectionFiles = [
   "sections/01-hero.html",
-  "sections/02-about.html",
-  "sections/03-research.html",
   "sections/04-publications.html",
   "sections/05-highlights.html",
   "sections/06-contact.html",
